9139f8525e1e7d6a3eb3f38241992a66815d7451
hamxamehboob <hamzamehboob2003@gmail.com>
2023-05-03 00:32:43 +0500

    Images added in Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,8 @@
+import 'package:chat_app/widgets/splash_screen_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,40 +10,79 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
+final _controller = PageController();
+
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     double screenwidth = MediaQuery.of(context).size.width;
     double screenheight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children:  [
-          Padding(
-            padding: EdgeInsets.only(top: 88,left: 53),
-            child: Text(
-              "Get Closer To",
-              style: TextStyle(
-                  color: Colors.black, fontSize: 36, fontWeight: FontWeight.w600),
-            ),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 88, left: 23),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Get Closer To",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "EveryOne",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: screenheight * 0.009,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 98),
+                child: Text(
+                  "Helps you to contact everyone with just easy way",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 23),
+                child: SizedBox(
+                  height: 400,
+                  child: PageView(
+                    controller: _controller,
+                    children: [
+                      Image.asset("assets/images/Splash_image1.png"),
+                      Image.asset("assets/images/Splash_image2.png"),
+                      Image.asset("assets/images/Splash_image3.jpg"),
+                      Image.asset("assets/images/Splash_image4.png"),
+                    ],
+                  ),
+                ),
+              ),
+              // Center(
+              //   child: SmoothPageIndicator(
+              //     controller: _controller,
+              //     count: 4,
+              //     effect: JumpingDotEffect(
+              //         dotColor: Colors.grey,
+              //         activeDotColor: Color(0xFF771F98),
+              //         jumpScale: 2,
+              //         dotHeight: 15,
+              //         verticalOffset: 20),
+              //   ),
+              // ),
+              SizedBox(
+                height: screenheight * 0.03,
+              ),
+              SplashScreenButton(),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 53),
-            child: Text(
-              "EveryOne",
-              style: TextStyle(
-                  color: Colors.black, fontSize: 36, fontWeight: FontWeight.w600),
-            ),
-          ),
-          SizedBox(height: screenheight*0.009,),
-          Padding(
-            padding: EdgeInsets.only(left: 23),
-            child: Text(
-              "Helps you to contact everyone with just easy way",
-              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/splash_screen_button.dart
@@ -0,0 +1,34 @@
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreenButton extends StatelessWidget {
+  const SplashScreenButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => Login(),
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.only(left: 90, right: 90, top: 8, bottom: 8),
+          decoration: BoxDecoration(
+              color: Color(0xFF771F98),
+              borderRadius: BorderRadius.circular(15)),
+          child: const Text(
+            "Get Started",
+            style: TextStyle(
+                color: Color(0xFFF3F3F3),
+                fontSize: 20,
+                fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
+    );
+  }
+}
